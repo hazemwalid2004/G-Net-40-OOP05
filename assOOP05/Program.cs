39cabb9a76ceb9b5d8a6bc5d79a956309b22502b
Hazem walid <walidf106@gmail.com>
2026-03-05 12:02:59 +0200

    Q2_part1

--- assOOP05/Program.cs
+++ assOOP05/Program.cs
@@ -13,6 +13,14 @@
             //Mention at least three benefits of using interfaces.
             //loose coupling , use polymorphism without inhertance and we can use multiple interface
             #endregion
+            #region Q2_part1
+            //a) What is the problem with this design? Both interfaces have a method called Greet() — how does the class handle it currently?
+            //method name conflict / when we call method with both interface print the same thing 
+            //b) How would you fix this so IEnglishSpeaker.Greet() says "Hello" and IArabicSpeaker.Greet() says "Ahlan"? What is this technique called?
+            // we use explicit interface 
+            //c) After applying your fix, can you call Greet() directly on a Translator object (e.g. translator.Greet())? Why or why not? How do you call each version?
+            //no / becouse  explicit members are hidden from the class // use interface as a variable type then call
+            #endregion
         }
     }
 }
