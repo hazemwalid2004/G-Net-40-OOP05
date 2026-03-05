9b6bdd63153bc73f1e729ac973f3179df4cff3b0
Hazem walid <walidf106@gmail.com>
2026-03-05 17:07:39 +0200

    Part2

--- assOOP05/Program.cs
+++ assOOP05/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace assOOP05
+﻿using assOOP03;
+
+namespace assOOP05
 {
     internal class Program
     {
@@ -30,6 +32,40 @@
             //deep ->objects must be isolated, modifications should not affect original
             //What is the risk of using a shallow copy when the object has reference-type fields?
             //the objects share the inner referance so any change in referance change the two objects 
+            #endregion
+            #region Q4_part1
+            //Console.WriteLine($"{e1.Title} - {e1.Dept.Name}");
+            //Dev / Testing
+            //Console.WriteLine($"{e2.Title} - {e2.Dept.Name}");
+            //QA / Testing
+            #endregion
+            #region Part2
+            Cinema cinema = new Cinema();
+            cinema.OpenCinema();
+            StandardTicket standTicket = new StandardTicket("Inception", 150.00m, "A-5");
+            VIPTicket vIP = new VIPTicket("Avengers", 200.00m, true);
+            IMAXTicket iMAX = new IMAXTicket("Dune", 180.00m, false);
+            standTicket.book();
+            vIP.book();
+            iMAX.book();    
+            cinema.AddTicket(standTicket);
+            cinema.AddTicket(vIP);
+            cinema.AddTicket(iMAX);
+            cinema.Print();
+            Console.WriteLine("--- Clone Test ---");
+            VIPTicket ticket = (VIPTicket)vIP.Clone();
+            ticket.MovieName = "Interstaller";
+            Console.Write("Orignal: ");
+            vIP.Print();
+            Console.Write("Clone    : ");
+            ticket.Print();
+            Console.WriteLine("------After Canceltion------");
+            standTicket.cancel();
+            standTicket.Print();
+            Console.WriteLine("--- BookingHelper.PrintAll ---");
+            BookingHelper booking = new BookingHelper();
+            booking.utility(new Iprintable[] { standTicket, vIP, iMAX});
+            cinema.CloseCinema();
             #endregion
         }
     }
---
+++ assOOP05/StandardTicket.cs
@@ -0,0 +1,29 @@
+﻿using assOOP05;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assOOP03
+{
+    internal class StandardTicket:Ticket,ICloneable
+    {
+        public string seatNumber { get; set; }
+        public StandardTicket(string moviename,decimal pri,string _seatnumber) : base(moviename, pri)
+        {
+            seatNumber=_seatnumber;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($" | Standard | seat : {seatNumber}");
+        }
+
+        public object Clone()
+        {
+            return new StandardTicket(MovieName,price,seatNumber);
+        }
+    }
+}
