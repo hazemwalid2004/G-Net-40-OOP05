1a31e98ea59b5a43d0dd40befc6b0cda3fcc0a16
Hazem walid <walidf106@gmail.com>
2026-03-05 11:52:21 +0200

    Q1_Part1

--- assOOP05/Program.cs
+++ assOOP05/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            #region Q1_Part1
+            // What is an interface in C#?
+            //it's a contract
+            //Why do we use interfaces instead of depending on concrete classes directly?
+            //because it's make a loose couplaing and easy to test 
+            //Mention at least three benefits of using interfaces.
+            //loose coupling , use polymorphism without inhertance and we can use multiple interface
+            #endregion
         }
     }
 }
